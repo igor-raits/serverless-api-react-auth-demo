8e6d74a6ae6af84ad861d96acbf5d62f3cf8455b
Igor Raits <igor.raits@gmail.com>
2025-08-04 23:49:23 +0200

    feat: clarify documentation and checks for the vars

--- variables.tf
+++ variables.tf
@@ -11,19 +11,22 @@ variable "aws_profile" {
 }
 
 variable "apigw_cors_allow_origins_extra" {
-  description = "Additional origins to allow for CORS (e.g., localhost for development)"
+  description = "Additional CORS allowed origins for API Gateway (e.g., http://localhost:5173 for development)"
   type        = list(string)
   default     = []
+  nullable    = false
 }
 
 variable "cognito_callback_urls_extra" {
   description = "Additional OAuth callback URLs (e.g., http://localhost:5173/callback for development)"
   type        = list(string)
   default     = []
+  nullable    = false
 }
 
 variable "cognito_logout_urls_extra" {
   description = "Additional OAuth logout URLs (e.g., http://localhost:5173/ for development)"
   type        = list(string)
   default     = []
+  nullable    = false
 }
